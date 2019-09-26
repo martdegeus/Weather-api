332909d08c23ae28da98ea3c913a79691551951a
Mart de Geus <martdegeus@gmail.com>
2019-09-26 11:59:04 +0200

    SCSS and html
    styling en html aangepast zodat het gemaakt is op de correcte manier

--- public/lib/main.js
+++ public/lib/main.js
@@ -2,27 +2,26 @@
 const key = "cac67298b97190e436286c4e1500ac73";
 const buttonArray = document.querySelector(".main__button");
 // Get the modal
-var modal = document.getElementById("header__submit");    
-// Get the button that opens the modal
-var btn = document.getElementById("button");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("header__submit_close")[0];
-// When the user clicks the button, open the modal 
+let modal = document.getElementById("header__submit");    
+// Modal opener and closer
+let btn = document.getElementById("button");
+let span = document.getElementsByClassName("header__submit_close")[0];
+ 
 btn.onclick = function() {
   modal.style.display = "block";
 }
-// When the user clicks on <span> (x), close the modal
+
 span.onclick = function() {
   modal.style.display = "none";
 }
-// When the user clicks anywhere outside of the modal, close it
+
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
 
-//geolocation starts
+// Geolocation starts
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(displayLocationInfo);
 }
@@ -30,12 +29,10 @@ if (navigator.geolocation) {
 function displayLocationInfo(position) {
   const lng = position.coords.longitude;
   const lat = position.coords.latitude;
-
-  console.log(localStorage);
   weatherBalloon(lat, lng);
 }
 
-//Get current location if correct draw it
+// Get current location if correct draw it
 function weatherBalloon(lat,lng) {
   fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lng + '&appid=' + key + '&units=metric')  
   .then(function(resp) { return resp.json() }) // Convert data to json
@@ -49,31 +46,32 @@ function weatherBalloon(lat,lng) {
       <h2 class="current_location__title">Current Location -</h2>
       <h2 class="current_location__title-name">${data.name}</h2>
       <img src="http://openweathermap.org/img/wn/${iconCode}@2x.png" class="current_location__icon">
-      <p class="current_location__temp--max">${maxcelcius}&#8451;</p>
-      <p class="current_location__temp--min">${mincelcius}&#8451;</p>
+      <p class="current_location__temp">${maxcelcius}&#8451;</p>
+      <p class="current_location__temp">${mincelcius}&#8451;</p>
       `;
   })
   .catch(function() {
   });
 }
 
-//delete all city's
+// Delete all city's
 let deleteLocal = document.querySelector('.current_location__delete');
  deleteLocal.addEventListener("click", function() {
    localStorage.clear();
    location.reload();
 });
 
-//local storage
+// Local storage
 localStorage = window.localStorage;
 
+// If city added
 buttonArray.addEventListener("click", function() {
   let boxvalue = document.querySelector('.main__search').value;
   localStorage.setItem("cityNames-" + boxvalue, boxvalue);
   location.reload();
 })
 
-//loop and draw city's
+// Loop and draw city's
 function drawcityWeather() {
   for (i = 0; i < localStorage.length; i++) {
     const container = document.querySelector(".locations");
@@ -81,16 +79,22 @@ function drawcityWeather() {
     localnames = localnames.replace(/ /g,"");
 
     container.innerHTML += `
-      <section class="location locations__item ${localnames}">
+      <section class="location locations__item ${localStorage.key(i)}">
       </section>
       `;
     let currentI = localStorage.getItem(localStorage.key(i));
-    //fetch api
+    // Fetch api
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${currentI}&appid=${key}&units=metric`)
     .then(result => {
-        return result.json();
+      // Remove from local storage if invalid
+      if (result.status !== 200) {
+        localStorage.removeItem("cityNames-" + currentI);
+        location.reload();
+      }
+
+      // If valid
+      return result.json();
     }).then(result => {
-      if(result.cod == 200){
         namespace = result.name.replace(/ /g,"");
         const container = document.querySelector('.cityNames-' + namespace);
         let maxcelcius = result.main.temp_max;
@@ -98,20 +102,28 @@ function drawcityWeather() {
         let iconCode = result.weather[0].icon;
 
         container.innerHTML += `
+        <input type="image" src="../images/trashcan.png" data-city="${currentI}" class="location__delete">
           <h2 class="location__title">${namespace}</h2>
           <img src="http://openweathermap.org/img/wn/${iconCode}@2x.png" class="location__icon">
-            <p class="location__temp">Max temp ${maxcelcius}&#8451;</p>
-            <p class="location__temp">Min temp ${mincelcius}&#8451;</p>
+          <p class="location__temp">Max temp ${maxcelcius}&#8451;</p>
+          <p class="location__temp">Min temp ${mincelcius}&#8451;</p>
           `;
-      }
-      else{
-        console.log("error");
-        localStorage.removeItem("cityNames-" + result.name);
-        console.log(localStorage);
-        //location.reload();
-      }
+        
+          removeCity();
     })
+  }
 }
+
+// Delete button
+function removeCity(){ 
+  let deleteButton = document.querySelectorAll(".locations__delete");
+  for (a = 0; a < deleteButton.length; a++) {
+      deleteButton[a].addEventListener('click', function () {
+          let data = this.getAttribute('data-city');
+          localStorage.removeItem("cityNames-" + data);
+          location.reload();
+      })
+  }
 }
 
 drawcityWeather();
